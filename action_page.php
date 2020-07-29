8eee45a6edf7a30db3b400f1315002f29bc7c4e7
Bryan Rose <roseb5@wit.edu>
2020-07-29 14:55:45 -0400

    Test

--- action_page.php
+++ action_page.php
@@ -5,14 +5,14 @@
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
 
-	$email_from = 'roseb5@wit.edu';
-	$email_subject = "New Form Submission";
-	$email_body = "You have recieved a message from the user $name.\n".
-					"Subject: $subject\n"
-					"Message: $message\n".
+	//$email_from = 'roseb5@wit.edu';
+	//$email_subject = "New Form Submission";
+	//$email_body = "You have recieved a message from the user $name.\n".
+	//				"Subject: $subject\n"
+	//				"Message: $message\n".
 	$to = "roseb5@wit.edu";
-	$headers = "From: $email_from \r\n";
-	$headers .= "Reply-To: $email \r\n";
-	mail($to, $email_subject, $email_body, $headers);
+	//$headers = "From: $email_from \r\n";
+	//$headers .= "Reply-To: $email \r\n";
+	mail($to,$subject, $email_body, $headers);
 	header('Location: home.html');
 ?>
