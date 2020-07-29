160f0d8c46680f90e2cdc8e56eb3b61248573fe6
Bryan Rose <roseb5@wit.edu>
2020-07-29 14:24:15 -0400

    Testing Contact

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+
+	$email_from = 'roseb5@wit.edu';
+	$email_subject = "New Form Submission";
+	$email_body = "You have recieved a message from the user $name.\n".
+					"Subject: $subject\n"
+					"Message: $message\n".
+	$to = "roseb5@wit.edu";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-To: $email \r\n";
+	mail($to, $email_subject, $email_body, $headers);
+	header('Location: home.html');
+?>
